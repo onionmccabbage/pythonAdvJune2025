7b13c3286f53aeefab31da9978745afb4617152d
onionmccabbage <onionmccabbage@live.com>
2025-06-17 12:59:53 +0100

    examples of testing python code

---
+++ using_tests/using_doctest.py
@@ -0,0 +1,22 @@
+# doctest is built in to Python
+import doctest
+
+
+def cubes(x):
+    # we assert the expected outcome
+    '''return the cube of x
+    >>> cubes(3)
+    27
+    >>> cubes('3')
+    TypeError: x must be numeric
+    '''
+    if type(x) in (int, float):
+        return x**3
+    else:
+        raise TypeError('x must be numeric')
+
+if __name__ == '__main__':
+    # c = cubes(3)
+    # print(c) # 27
+    # e = cubes('3')
+    doctest.testmod()
