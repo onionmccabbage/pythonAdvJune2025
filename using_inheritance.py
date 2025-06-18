2d9e09ff479ac502fd96301e636ac91712b6c6ac
onionmccabbage <onionmccabbage@live.com>
2025-06-18 10:29:29 +0100

    using class inheritance

---
+++ using_inheritance.py
@@ -0,0 +1,23 @@
+# classes may inherit from a parent (super)
+
+class Parliamentarian():
+    def __init__(self, name):
+        self.name = name
+
+class MP(Parliamentarian):
+    '''We will also encode which party'''
+    def __init__(self, name, party):
+        Parliamentarian.__init__(name)
+        self.party = party
+
+class CivilServant(Parliamentarian):
+    '''we will encode the role'''
+    def __init__(self, name, role):
+        super().__init__(name)
+        self.role = role
+
+if __name__ == '__main__':
+    # use our classes
+    betty = Parliamentarian('Betty Boothroyd')
+    lloyd = MP('Lloyd George', 'Liberal')
+    humph = CivilServant('Humphrey', 'AI')
